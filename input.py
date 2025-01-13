d25fba6d71530f133d256fb6274952482eac2e78
Adeline <adeline.patenne@hotmail.fr>
2025-01-13 19:09:30 +0100

    fix: check if input is valid with conditions and loops

--- input.py
+++ input.py
@@ -1,9 +1,17 @@
 import re
 def input_user():
-    input_string = input("Veuillez entrer votre opération : ")
-    numbers = re.findall(r'\d+', input_string)
-    operators = re.findall(r'\D', input_string)
-    operators.append("") # adding one caracter to the list so length of numbers and operators are equals
+    input_string = input("Veuillez entrer votre opération : ").replace(" ", "")
+    if bool(re.search(r'\d', input_string)) == False:
+        print("Vous n'avez entré aucun chiffre")
+        return input_user()
+    else:
+        numbers = re.findall(r'\d+', input_string)
+        operators = re.findall(r'[+-/*]+', input_string)
+        
+        new_operators = operators
+        new_operators.append("") # adding one caracter to the list so length of numbers and operators are equals
+
+    print(f" ceci est un test de input_string {input_string}")
 
     new_list_numbers = []
     for number in numbers:
@@ -11,71 +19,54 @@ def input_user():
         new_list_numbers.append(number)
 
     new_string = []
+    # if len(numbers) >= len(operators):
+    print(f"OPERATEUR {operators}")
+    print(f"NEW OPERATEUR {new_operators}")
+    print(f"NUMBERS {numbers}")
     for index in range(len(new_list_numbers)):
-        new_string.append(new_list_numbers[index])
-        new_string.append(operators[index])
-    new_string.pop() # removing empty space at the end
-    print(new_string)
-input_user()
-
-
-
-
-
-
-"""
-# def evaluate_expression(arr):
-#     expression = ''.join(arr)
-#     result = eval(expression)
-#     return result
-# arr = [input("number : "), input("operator :"), input("number : ")]
-# result = evaluate_expression(arr)
-# print(result)  # Output: 4
-
-# arr = ['(', '1', '6', '*', '0', ')', '+', '(', '1', '6', '*', '1', ')', '+', '5']
-# result = evaluate_expression(arr)
-# print(result)  # Output: """
-"""
-def test():
-    # calcul = ['(', '1', '6', '*', '0', ')', '+', '(', '1', '6', '*', '1', ')', '+', '5']
-    calcul = input("veuillez entrer votre opération : ")
-    line = []
-    for caracter in range(len(calcul)):
-        # while True:
-        #     try: test = int(calcul[caracter])
-        #     except Exception:
-        #         break
-        #     calcul[caracter] = int(calcul[caracter])
-        #     break
-        # if calcul[caracter] == re.findall(r'\d+', calcul[caracter]):
-        #     calcul[caracter] = int(calcul[caracter])
-
-        while True:
-            try: test = int(calcul[caracter])
-            except Exception:
-                calcul[caracter]
-                break
-            number = int(calcul[caracter])
-            break
-
-        if calcul[caracter] == number:
-            # ligne += number
-            line.append(number)
-            print(number)
+        if bool(re.search(r'^[+-/*]', input_string)):
+            new_string.append(operators[index])
+            new_string.append(new_list_numbers[index]) 
         else:
-            line.append(calcul[caracter])
+            new_string.append(new_list_numbers[index])
+            new_string.append(operators[index])
+            # new_string.pop() # removing empty space at the end
+    
+    if new_string[-1] == "":
+        new_string.pop()
+    # else:
+    #     for index in range(len(new_list_numbers)):
+    #         new_string.append(operators[index])
+    #         new_string.append(new_list_numbers[index])         
 
-        # elif calcul[caracter] == operator:
-        #     line.append(operator)
+    print(f"ceci est la nouvelle liste finie {new_string}")
 
-    for caracter in line:
-        match caracter:
-            case "+":
-                caracter="+"
-            case re.findall(r'\d+', caracter):
-                caracter = int(caracter)
-    result = line
-    print(line)
-    # print(result)
-"""
+   
+    return new_string
+# input_user()
 
+
+# def check_number():
+def check_operator(operation_list):
+    for index in range(len(operation_list)):
+        result = 0
+        if operation_list[index].isnumeric() and operation_list[index+1]%2 != 0:
+            match operation_list[index]:
+                case "+":
+                    result += operation_list[index]
+                case "-":
+                    result -= operation_list[index]
+                case "*":
+                    result *= operation_list[index]
+                case "/":
+                    result /= operation_list[index]
+                case "%":
+                    result %= operation_list[index]
+                case _:
+                    print(f"L'opérateur {operation_list[index]} n'est pas valide")
+        print(result)
+
+while True:
+
+    calcul_list = input_user()
+    # check_operator(calcul_list)
