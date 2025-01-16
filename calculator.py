b5e0ee71156e3aea1f49f85889f0b5fc75ee9e35
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-01-16 14:32:22 +0100

    FEAT: center text on terminal

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ def u_input(): #demand mathematical expression from user
 
     
 
-    start=input("type in your calculation ('q' to power off)>> ").strip()
+    start=input("{:>100}".format("type in your calculation ('q' to power off)>>")).strip().upper()
 
     operationB=re.findall(r"[\d]+|[\D]", start)
 
@@ -31,7 +31,7 @@ def u_input(): #demand mathematical expression from user
                     operationA.append(element)
                 case ")":
                     operationA.append(element)
-                case "q":
+                case "Q":
                     pass
                 case _:
                     print(f"{element} ain't no operator bruh")
@@ -44,12 +44,15 @@ def u_input(): #demand mathematical expression from user
                 
             case '-':
                 operationA.insert(0, float(0))
-            case 'q':
-                print("powering off...")
+            case 'Q':
+                print("{:>85}".format("power off..."))
                 exit()    
             case _:
                 print(f"DON'T START WITH THAT DISGUSTING {operationB[0]}")
-                operationA.pop(0)
+                try:
+                    operationA.pop(0)
+                except IndexError:
+                    u_input()
     
     
     return operationA 
@@ -168,23 +171,27 @@ def modulo(operation):
 
 
 def main():
-    while True:
-        operation=u_input()
-        while len(operation)>1:
+    try:
+        while True:
+            operation=u_input()
+            while len(operation)>1:
 
-            if '*' in operation:
-                result=multiplication(operation)
-            elif '/' in operation:
-                result=division(operation)
-            elif '%' in operation:
-                result=modulo(operation)
-            else:
-                for element in operation:
-                    match element:
-                        case '+':
-                            result=addition(operation)
-                        case '-':
-                            result=subtraction(operation)
-        print(result)
+                if '*' in operation:
+                    result=multiplication(operation)
+                elif '/' in operation:
+                    result=division(operation)
+                elif '%' in operation:
+                    result=modulo(operation)
+                else:
+                    for element in operation:
+                        match element:
+                            case '+':
+                                result=addition(operation)
+                            case '-':
+                                result=subtraction(operation)
+            print("{:>80}".format(str(result)))
+    except UnboundLocalError:
+        print("{:>85}".format("result unavailable"))
+        main()
 
 main()
