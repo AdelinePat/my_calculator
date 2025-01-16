9fca95e64ba17316218bfedf18467566c717746e
Adeline <adeline.patenne@hotmail.fr>
2025-01-16 13:00:46 +0100

    fix: input_user return error if input empty and if enter a wrong number format
     Use regex accepting number with multiple . inside one number, return error if this kind of number is entered

--- input.py
+++ input.py
@@ -1,12 +1,21 @@
 import re, sys
 def input_user():
     input_string = input("Veuillez entrer votre opération : ").replace(" ", "")
-    clean_string = re.findall(r'(?:[0-9.]+)|(?:[+/*%\(\)-])', input_string) #TODO /!\ this regex accept a number 10.4.5.6.6
-    # clean_string = re.findall(r'((?:[0-9]+).(?:[0-9]+))|(?:[+/*%-])', input_string) #VERSION 2
-    # clean_string = re.findall(r'(\d+\.\d+)|(?:[0-9]+)|(?:[+/*%-])', input_string)
-    # ((?:[0-9]+).(?:[0-9]+))|(?:[+/*%-])
-    
-    # clean_string = re.findall(r'(?:\d+)|(?:[+/*%\(\)-])', input_string) # regex with () 
+    clean_string = re.findall(r'(?:[0-9.]+)|(?:[+/*%\(\)-])', input_string) # /!\ this regex accept a number 10.4.5.6.6
+
+    try:
+        if len(clean_string) < 1: # Check if input is empty
+            raise Exception("Vous n'avez pas entré d'opération")
+    except Exception as message:
+        print(message)
+
+    for element in clean_string:
+        try:
+            test = re.findall(r"\.+", element) # Correct clean_string in case a number 10.4.5.6 is entered
+            if len(test) > 1:
+                print(f"{element} n'est pas un nombre !") 
+        except Exception:
+            pass
 
     try:
         match = re.search(r'(?:[^0-9.+/*%\(\)-]+)', input_string)
@@ -18,8 +27,7 @@ def input_user():
         # return None
         sys.exit(1)
 
-    #TODO : raise exception when input is empty !! 
-    #TODO : raise exception if there is something that looks like 10.9.93.5.0 and return error to user
+
     #TODO : raise exception if input doesn't start with number or + or -
     #TODO : max size of list
         # print(clean_string)
@@ -28,7 +36,7 @@ def input_user():
 
 # input_user()
 
-# test =  input_user()
+test =  input_user()
 # blabla = "".join(test)
 # print(test)
 # print(blabla)
